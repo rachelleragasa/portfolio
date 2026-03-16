daebef1ca4ff8c68516350077d192c394eeceea4
Rachelle Ragasa <rachelleragasa.costa@gmail.com>
2026-03-16 14:53:04 +0000

    add umami analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+if (import.meta.env.PROD) {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = "https://cloud.umami.is/script.js";
+  script.setAttribute("data-website-id", import.meta.env.VITE_UMAMI_WEBSITE_ID);
+  document.head.appendChild(script);
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
