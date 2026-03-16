a7cc721a50154bb74fcf085c937681839db3b638
Rachelle Ragasa <rachelleragasa.costa@gmail.com>
2026-03-16 01:22:04 +0000

    test if env vars are being pulled correctly

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -20,6 +20,11 @@ function Contact() {
   const onSubmit = async (data: ContactFormData) => {
     setStatus("sending");
     try {
+      console.log({
+        service: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        template: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        key: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+      });
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
